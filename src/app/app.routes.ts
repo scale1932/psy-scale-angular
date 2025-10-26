fe625a4915597b9550257cbf4d39fd231aefc943
Adam Zhang <psy_zhangxin@outlook.com>
2025-10-26 11:02:46 +0800

    e2e 测试

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,9 @@ import {Home} from './pages/home/home';
 import {NotFound} from './pages/error/not-found/not-found';
 import {About} from './pages/about/about';
 import {AuthGuard} from './auth/guards/auth.guard';
-import {AuthComponent} from './auth/login/auth';
 
 // 路由嵌套，注意这里的 path，永远不要以 / 开头
 export const routes: Routes = [
-  {
-    path: 'login',
-    component: AuthComponent
-  },
   {path: '', component: Home},
   {path: 'home', component: Home, canActivate: [AuthGuard]},
   {path: 'about', component: About, canActivate: [AuthGuard]},
